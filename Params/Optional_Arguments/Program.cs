b6a3465bd8bf1b3f7e81eb65cc57d6390f595cee
Beka_Kavlelashvili <beqakavlela2001@gmail.com>
2023-03-30 14:46:44 +0400

    optional arguments

---
+++ Params/Optional_Arguments/Program.cs
@@ -0,0 +1,25 @@
+﻿class UseOptArgs
+{
+    static void Display(string str, int start = 0, int stop = -1)
+    {
+        if (stop < 0)
+        {
+            stop = str.Length;
+        }
+
+        if (stop > str.Length | start > stop | start < 0)
+            return;
+
+        for (int i = start; i < stop; i++)
+            Console.Write(str[i]);
+
+        Console.WriteLine();
+    }
+
+    static void Main()
+    {
+        Display("This is a test");
+        Display("This is a test", 10);
+        Display("This is a test", 5, 12);
+    }
+}
