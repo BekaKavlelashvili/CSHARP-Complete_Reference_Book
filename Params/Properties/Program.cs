34815355b828aad7ce5cfdea7da0809e93863ef4
Beka_Kavlelashvili <beqakavlela2001@gmail.com>
2023-04-05 14:39:00 +0400

    properties

---
+++ Params/Properties/Program.cs
@@ -0,0 +1,35 @@
+﻿class SimpProp
+{
+    int prop;
+
+    public SimpProp() { prop = 0; }
+
+    public int MyProp
+    {
+        get
+        {
+            return prop;
+        }
+        set
+        {
+            if (value >= 0) prop = value;
+        }
+    }
+}
+
+class PropDemo
+{
+    static void Main()
+    {
+        SimpProp ob = new SimpProp();
+
+        Console.WriteLine("Original value of ob.MyProp: " + ob.MyProp);
+
+        ob.MyProp = 100;
+        Console.WriteLine("Value of ob.MyProp: " + ob.MyProp);
+
+        Console.WriteLine("Attempting to assign -10 to ob.MyProp");
+        ob.MyProp = -10;
+        Console.WriteLine("Value of ob.MyProp: " + ob.MyProp);
+    }
+}
