6954b8b964ae8f7e98e4fc6449afaf88ddaa1a21
Beka_Kavlelashvili <beqakavlela2001@gmail.com>
2023-04-11 13:45:13 +0400

    interface properties

---
+++ Params/Interface_Properties/Program.cs
@@ -0,0 +1,48 @@
+﻿public interface ISeries
+{
+    int Next { get; set; }
+}
+
+class ByTwos : ISeries
+{
+    int val;
+
+    public ByTwos()
+    {
+        val = 0;
+    }
+
+    public int Next
+    {
+        get
+        {
+            val += 2;
+            return val;
+        }
+        set
+        {
+            val = value;
+        }
+    }
+}
+
+class SeriesDemo
+{
+    static void Main()
+    {
+        ByTwos byTwos = new ByTwos();
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("Next value is " + byTwos.Next);
+        }
+
+        Console.WriteLine("\nStarting at 21");
+        byTwos.Next = 21;
+
+        for (int i = 0; i < 5; i++)
+        {
+            Console.WriteLine("Next value is " + byTwos.Next);
+        }
+    }
+}
