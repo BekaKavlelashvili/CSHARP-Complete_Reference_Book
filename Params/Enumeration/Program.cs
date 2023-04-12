1389abe1785c168c583f20eefaceb3b9f9cfea30
Beka_Kavlelashvili <beqakavlela2001@gmail.com>
2023-04-12 12:43:36 +0400

    enumeration

---
+++ Params/Enumeration/Program.cs
@@ -0,0 +1,36 @@
+﻿class EnumDemo
+{
+    enum Apple
+    {
+        Jonathan,
+        GoldenDel,
+        RedDel,
+        Winesap,
+        Cortland,
+        McIntosh
+    }
+
+    static void Main()
+    {
+        string[] color =
+        {
+            "Red",
+            "Yellow",
+            "Red",
+            "Red",
+            "Red",
+            "Reddish Green",
+        };
+
+        Apple i;
+        for (i = Apple.Jonathan; i <= Apple.McIntosh; i++)
+        {
+            Console.WriteLine(i + "has value of " + (int)i);
+        }
+
+        Console.WriteLine();
+
+        for (i = Apple.Jonathan; i <= Apple.McIntosh; i++)
+            Console.WriteLine("Color of " + i + " is " + color[(int)i]);
+    }
+}
