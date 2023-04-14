8830fce9ad0304077e20d94d45e524f3a41913ff
Beka_Kavlelashvili <beqakavlela2001@gmail.com>
2023-04-14 15:31:02 +0400

    exception class members

---
+++ Params/Exception_Class_Members/Program.cs
@@ -0,0 +1,38 @@
+﻿class ExcTest
+{
+    public static void GenException()
+    {
+        int[] nums = new int[4];
+
+        Console.WriteLine("Before exception is generated");
+
+        for (int i = 0; i < 10; i++)
+        {
+            nums[i] = i;
+            Console.WriteLine("nums[{0}]: {1}", i, nums[i]);
+        }
+
+        Console.WriteLine("This won't be displayed");
+    }
+}
+
+class UseExcept
+{
+    static void Main()
+    {
+        try
+        {
+            ExcTest.GenException();
+        }
+        catch (IndexOutOfRangeException ex)
+        {
+            Console.WriteLine("Starndard message is: ");
+            Console.WriteLine(ex);
+            Console.WriteLine("Stack trace: " + ex.StackTrace);
+            Console.WriteLine("Message: " + ex.Message);
+            Console.WriteLine("TargetSite: " + ex.TargetSite);
+        }
+
+        Console.WriteLine("After catch block");
+    }
+}
