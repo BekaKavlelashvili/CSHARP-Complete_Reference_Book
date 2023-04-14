951ab8edd87d78c59aa95dbc20cb67191ccdbc5a
Beka_Kavlelashvili <beqakavlela2001@gmail.com>
2023-04-14 13:06:03 +0400

    exception handling

---
+++ Params/Exception_Handling/Program.cs
@@ -0,0 +1,35 @@
+﻿class ExcTest
+{
+    public static void GenException()
+    {
+        int[] nums = new int[4];
+
+        Console.WriteLine("Before exception is generated");
+
+        for (int i = 0; i < 10; i++)
+        {
+            nums[i] = i;
+            Console.WriteLine("nums[{0}]: {1}", i, nums[i]);
+        }
+
+        Console.WriteLine("This won't be displayed");
+    }
+}
+
+class ExcDemo
+{
+    static void Main()
+    {
+        try
+        {
+            ExcTest.GenException();
+        }
+        catch (IndexOutOfRangeException)
+        {
+
+            Console.WriteLine("Index out-of-bounds");
+        }
+
+        Console.WriteLine("After catch block");
+    }
+}
