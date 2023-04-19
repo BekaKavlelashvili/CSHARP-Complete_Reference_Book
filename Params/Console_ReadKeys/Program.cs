830d18af54dc28408adc4cde7f13ed8ab73f8d7d
Beka_Kavlelashvili <beqakavlela2001@gmail.com>
2023-04-19 19:46:18 +0400

    Console.ReadKey method

---
+++ Params/Console_ReadKeys/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography;
+
+class ReadKeys
+{
+    static void Main()
+    {
+        ConsoleKeyInfo keyPress;
+
+        Console.WriteLine("Enter keystrokes. Enter Q to stop.");
+
+        do
+        {
+            keyPress = Console.ReadKey();
+
+            Console.WriteLine(" Your key is: " + keyPress.KeyChar);
+
+            if ((ConsoleModifiers.Alt & keyPress.Modifiers) != 0)
+                Console.WriteLine("Alt key pressed.");
+            if ((ConsoleModifiers.Control & keyPress.Modifiers) != 0)
+                Console.WriteLine("Control key pressed.");
+            if ((ConsoleModifiers.Shift & keyPress.Modifiers) != 0)
+                Console.WriteLine("Shift key pressed.");
+        } while (keyPress.KeyChar != 'Q');
+    }
+}
