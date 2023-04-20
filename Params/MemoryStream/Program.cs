d71bd666086f315a9fced59cebe9982916c3a311
Beka_Kavlelashvili <beqakavlela2001@gmail.com>
2023-04-20 17:11:14 +0400

    memory stream

---
+++ Params/MemoryStream/Program.cs
@@ -0,0 +1,53 @@
+﻿class MemStrDemo
+{
+    static void Main()
+    {
+        byte[] storage = new byte[255];
+
+        MemoryStream memstr = new MemoryStream(storage);
+
+        StreamWriter memwtr = new StreamWriter(memstr);
+        StreamReader memrdr = new StreamReader(memstr);
+
+        try
+        {
+            for (int i = 0; i < 10; i++)
+                memwtr.WriteLine("byte [" + i + "]: " + i);
+
+            memwtr.WriteLine(".");
+
+            memwtr.Flush();
+
+            Console.WriteLine("Reading from storage directly: ");
+
+            foreach (char ch in storage)
+            {
+                if (ch == '.')
+                    break;
+                Console.Write(ch);
+            }
+
+            Console.WriteLine("\nReading through memrdr: ");
+
+            memstr.Seek(0, SeekOrigin.Begin);
+
+            string str = memrdr.ReadLine();
+            while (str != null)
+            {
+                str = memrdr.ReadLine();
+                if (str[0] == '.')
+                    break;
+                Console.WriteLine(str);
+            }
+        }
+        catch (IOException exc)
+        {
+            Console.WriteLine("I/O error\n" + exc.Message);
+        }
+        finally
+        {
+            memwtr.Close();
+            memrdr.Close();
+        }
+    }
+}
