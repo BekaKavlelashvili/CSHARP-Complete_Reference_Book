aece05e24fd14b636a35ca31ff261b098775ca16
Beka_Kavlelashvili <beqakavlela2001@gmail.com>
2023-04-20 13:13:26 +0400

    write to a file

---
+++ Params/Write_To_File/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualBasic;
+
+class WriteFile
+{
+    static void Main(string[] args)
+    {
+        FileStream fout = null;
+
+        try
+        {
+            fout = new FileStream("test.txt", FileMode.CreateNew);
+            for (char i = 'A'; i <= 'Z'; i++)
+            {
+                fout.WriteByte((byte)i);
+            }
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("I/O Error: " + e.Message);
+        }
+        finally
+        {
+            if (fout != null)
+                fout.Close();
+        }
+    }
+}
