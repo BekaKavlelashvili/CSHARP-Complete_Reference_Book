04b83563a4f2259f60c7351331a9434234306f8a
Beka_Kavlelashvili <beqakavlela2001@gmail.com>
2023-04-20 14:07:43 +0400

    write to a file with streamwriter

---
+++ Params/StreamWriter/Program.cs
@@ -0,0 +1,33 @@
+﻿class KtoD
+{
+    static void Main()
+    {
+        string str;
+        StreamWriter fstr_out = null;
+
+        try
+        {
+            fstr_out = new StreamWriter("test.txt");
+            Console.WriteLine("Enter text ('stop' to quit).");
+            do
+            {
+                Console.WriteLine(": ");
+                str = Console.ReadLine();
+                if (str != "stop")
+                {
+                    str = str + "\r\n";
+                    fstr_out.Write(str);
+                }
+            } while (str != "stop");
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("I/O error:\n" + e.Message);
+        }
+        finally
+        {
+            if (fstr_out != null)
+                fstr_out.Close();
+        }
+    }
+}
