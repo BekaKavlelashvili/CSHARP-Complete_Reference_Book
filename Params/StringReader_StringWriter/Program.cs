6611be5901451c5a43ff30b369951669bbb0c66e
Beka_Kavlelashvili <beqakavlela2001@gmail.com>
2023-04-21 16:12:44 +0400

    stringReader and stringWriter

---
+++ Params/StringReader_StringWriter/Program.cs
@@ -0,0 +1,36 @@
+﻿class StrRdrWrtDemo
+{
+    static void Main()
+    {
+        StringWriter strWrt = null;
+        StringReader strRdr = null;
+
+        try
+        {
+            strWrt = new StringWriter();
+
+            for (int i = 0; i < 10; i++)
+            {
+                strWrt.WriteLine("This is i: " + i);
+            }
+
+            strRdr = new StringReader(strWrt.ToString());
+            string str = strRdr.ReadLine();
+
+            while (str != null)
+            {
+                str = strRdr.ReadLine();
+                Console.WriteLine(str);
+            }
+        }
+        catch (IOException exc)
+        {
+            Console.WriteLine("I/O error\n" + exc.Message);
+        }
+        finally
+        {
+            if(strWrt != null) strWrt.Close();
+            if(strRdr!= null) strRdr.Close();
+        }
+    }
+}
