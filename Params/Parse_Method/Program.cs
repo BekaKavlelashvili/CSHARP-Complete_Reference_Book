8d378b1c6a5fe8e40aef883ad7eb66cc30998efd
Beka_Kavlelashvili <beqakavlela2001@gmail.com>
2023-04-22 12:55:48 +0400

    using of parse method

---
+++ Params/Parse_Method/Program.cs
@@ -0,0 +1,54 @@
+﻿class AvgNums
+{
+    static void Main()
+    {
+        string str;
+        int n;
+        double sum = 0.0;
+        double avg, t;
+
+        Console.WriteLine("How many numbers will you enter: ");
+        str = Console.ReadLine();
+
+        try
+        {
+            n = Int32.Parse(str);
+        }
+        catch (FormatException exc)
+        {
+            Console.WriteLine(exc.Message);
+            return;
+        }
+        catch (OverflowException exc)
+        {
+            Console.WriteLine(exc.Message);
+            return;
+        }
+
+        Console.WriteLine("Enter " + n + " values.");
+        for (int i = 0; i < n; i++)
+        {
+            Console.Write(": ");
+            str = Console.ReadLine();
+
+            try
+            {
+                t = Double.Parse(str);
+            }
+            catch (FormatException exc)
+            {
+                Console.WriteLine(exc.Message);
+                t = 0.0;
+            }
+            catch (OverflowException exc)
+            {
+                Console.WriteLine(exc.Message);
+                t = 0;
+            }
+            sum += t;
+        }
+
+        avg = sum / n;
+        Console.WriteLine("Avg is: " + avg);
+    }
+}
