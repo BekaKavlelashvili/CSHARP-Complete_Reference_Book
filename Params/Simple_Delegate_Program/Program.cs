afdcf8693700216727df74940457b6ddd9f07e2e
Beka_Kavlelashvili <beqakavlela2001@gmail.com>
2023-04-25 15:47:12 +0400

    delegate, method group conversion, refer to instance method

---
+++ Params/Simple_Delegate_Program/Program.cs
@@ -0,0 +1,59 @@
+﻿using System.Globalization;
+using System.Xml.Serialization;
+
+delegate string StrMod(string str);
+
+class DelegateTest
+{
+    static string ReplaceSpaces(string s)
+    {
+        Console.WriteLine("Replaces spaces with hyphens");
+        return s.Replace(' ', '-');
+    }
+
+    static string RemoveSpaces(string s)
+    {
+        string temp = string.Empty;
+        int i;
+
+        Console.WriteLine("Removing spaces");
+
+        for (i = 0; i < s.Length; i++)
+            if (s[i] != ' ')
+                temp += s[i];
+
+        return temp;
+    }
+
+    static string Reverse(string s)
+    {
+        string temp = string.Empty;
+        int i, j;
+
+        Console.WriteLine("Reversing string");
+        for (j = 0, i = s.Length - 1; i >= 0; i--, j++)
+            temp += s[i];
+
+        return temp;
+    }
+
+    static void Main()
+    {
+        StrMod strMod = new StrMod(ReplaceSpaces);
+        string str;
+
+        str = strMod("This is a test.");
+        Console.WriteLine("Resulting string: " + str);
+        Console.WriteLine();
+
+        strMod = new StrMod(RemoveSpaces);
+        str = strMod("This is a test.");
+        Console.WriteLine("Resulting string: " + str);
+        Console.WriteLine();
+
+        strMod = new StrMod(Reverse);
+        str = strMod("This is a test.");
+        Console.WriteLine("Resulting string: " + str);
+        Console.WriteLine();
+    }
+}
