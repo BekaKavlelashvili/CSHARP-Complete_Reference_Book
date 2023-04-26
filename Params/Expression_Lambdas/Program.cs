7d7e28e2f012f4146230a5935767ebc524949081
Beka_Kavlelashvili <beqakavlela2001@gmail.com>
2023-04-26 13:51:58 +0400

    expression lambdas

---
+++ Params/Expression_Lambdas/Program.cs
@@ -0,0 +1,27 @@
+﻿delegate int Incr(int v);
+
+delegate bool IsEven(int v);
+
+class SimpleLambdaDemo
+{
+    static void Main()
+    {
+        Incr incr = count => count + 2;
+
+        Console.WriteLine("Use incr lambda expression: ");
+        int x = -10;
+        while (x <= 0)
+        {
+            Console.Write(x + " ");
+            x = incr(x);
+        }
+
+        Console.WriteLine("\n");
+
+        IsEven isEven = num => num % 2 == 0;
+        Console.WriteLine("Use isEven lambda expression: ");
+        for (int i = 1; i <= 10; i++)
+            if (isEven(i))
+                Console.WriteLine(i + " is even.");
+    }
+}
