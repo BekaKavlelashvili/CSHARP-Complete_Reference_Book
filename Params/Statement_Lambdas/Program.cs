f7239222c9d22559f5ecf367bda045f2001ede9f
Beka_Kavlelashvili <beqakavlela2001@gmail.com>
2023-04-26 14:10:23 +0400

    statement lambdas

---
+++ Params/Statement_Lambdas/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Text.RegularExpressions;
+
+delegate string StrMod(string s);
+
+class UseStatementLambdas
+{
+    static void Main()
+    {
+        StrMod ReplaceSpaces = s =>
+        {
+            Console.WriteLine("Replacing spaces with hyphens.");
+            return s.Replace(' ', '-');
+        };
+
+        StrMod RemoveSpaces = s =>
+        {
+            string temp = string.Empty;
+            int i;
+
+            Console.WriteLine("Removing spaces.");
+            for (i = 0; i < s.Length; i++)
+                if (s[i] != ' ')
+                    temp += s[i];
+
+            return temp;
+        };
+
+        StrMod ReverseString = s =>
+        {
+            string temp = string.Empty;
+            int i, j;
+
+            Console.WriteLine("Reversing string.");
+            for (j = 0, i = s.Length - 1; i >= 0; i--, j++)
+                temp += s[i];
+
+            return temp;
+        };
+
+        string str;
+
+        StrMod strOp = ReplaceSpaces;
+        str = strOp("This is a test.");
+        Console.WriteLine("Resulting string: " + str);
+        Console.WriteLine();
+
+        strOp = RemoveSpaces;
+        str = strOp("This is a test.");
+        Console.WriteLine("Resulting string: " + str);
+        Console.WriteLine();
+
+        strOp = ReverseString;
+        str = strOp("This is a test.");
+        Console.WriteLine("Resulting string: " + str);
+    }
+}
