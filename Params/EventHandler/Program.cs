7a8d84cc41e87bb5764dfa24a22ce099d7b182d2
Beka_Kavlelashvili <beqakavlela2001@gmail.com>
2023-04-28 14:26:57 +0400

    using EventHandler delegate

---
+++ Params/EventHandler/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Runtime.CompilerServices;
+
+class MyEvent
+{
+    public event EventHandler SomeEvent;
+
+    public void OnSomeEvent()
+    {
+        if (SomeEvent != null)
+            SomeEvent(this, EventArgs.Empty);
+    }
+}
+
+class EventDemo
+{
+    static void Handler(object sender, EventArgs e)
+    {
+        Console.WriteLine("Event occurred");
+        Console.WriteLine("Source is " + sender);
+    }
+
+    static void Main()
+    {
+        MyEvent evt = new MyEvent();
+        evt.SomeEvent += Handler;
+
+        evt.OnSomeEvent();
+    }
+}
