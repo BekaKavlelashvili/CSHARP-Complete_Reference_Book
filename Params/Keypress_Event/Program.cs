47e78545dcd413eaa1591d569a9b6f9a8457d0b3
Beka_Kavlelashvili <beqakavlela2001@gmail.com>
2023-04-28 14:41:19 +0400

    key press event

---
+++ Params/Keypress_Event/Program.cs
@@ -0,0 +1,44 @@
+﻿class KeyEventArgs : EventArgs
+{
+    public char ch;
+}
+
+class KeyEvent
+{
+    public event EventHandler<KeyEventArgs> KeyPress;
+
+    public void OnKeyPress(char key)
+    {
+        KeyEventArgs k = new KeyEventArgs();
+
+        if (k != null)
+        {
+            k.ch = key;
+            KeyPress(this, k);
+        }
+    }
+}
+
+class KeyEventDemo
+{
+    static void Main()
+    {
+        KeyEvent kevt = new KeyEvent();
+        ConsoleKeyInfo key;
+        int count = 0;
+
+        kevt.KeyPress += (sender, e) => Console.WriteLine(" Received keystroke " + e.ch);
+
+        kevt.KeyPress += (sender, e) => count++;
+
+        Console.WriteLine("Enter some characters." + "Enter a period to stop.");
+
+        do
+        {
+            key = Console.ReadKey();
+            kevt.OnKeyPress(key.KeyChar);
+        } while (key.KeyChar != '.');
+
+        Console.WriteLine(count + " keys pressed.");
+    }
+}
