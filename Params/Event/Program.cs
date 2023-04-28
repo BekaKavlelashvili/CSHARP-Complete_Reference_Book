a61ad347cc42e9a63af87186cb6dfa550df2dcbf
Beka_Kavlelashvili <beqakavlela2001@gmail.com>
2023-04-28 13:29:40 +0400

    event simple example

---
+++ Params/Event/Program.cs
@@ -0,0 +1,28 @@
+﻿delegate void MyEventHandler();
+
+class MyEvent
+{
+    public event MyEventHandler SomeEvent;
+
+    public void OnSomeEvent()
+    {
+        if (SomeEvent != null)
+            SomeEvent();
+    }
+}
+
+class EventDemo
+{
+    static  void Handler()
+    {
+        Console.WriteLine("Event occurred");
+    }
+
+    static void Main()
+    {
+        MyEvent evt = new MyEvent();
+
+        evt.SomeEvent += Handler;
+        evt.OnSomeEvent();
+    }
+}
