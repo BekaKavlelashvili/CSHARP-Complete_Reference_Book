df594ed863134c40eebd50b47c9896014182a874
Beka_Kavlelashvili <beqakavlela2001@gmail.com>
2023-05-25 15:35:01 +0400

    unsafe, pointer and fixed

---
+++ Params/UnsafeCode/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Reflection.Metadata;
+using System.Xml;
+
+class UnsafeCode
+{
+	unsafe static void UseUnsafe()
+	{
+		Console.WriteLine("\n=== Use Unsafe ===");
+		int count = 99;
+		int* p; //create an int pointer
+
+		p = &count; // put count address in p pointer
+
+		Console.WriteLine("Initiale value of count is : " + *p);
+
+		*p = 10; // assign 10 to count via *p
+
+		Console.WriteLine("New value of count is: " + *p);
+	}
+
+	unsafe static void UseFixed()
+	{
+		Console.WriteLine("\n=== Use Fixed ===");
+
+		UseFixedCode fixedCode = new UseFixedCode(19);
+
+		fixed (int* p = &fixedCode.num)
+		{
+			Console.WriteLine("Initial value of fixedCode.Num is: " + *p);
+
+			*p = 10;
+
+			Console.WriteLine("New value of o.num is " + *p);
+		}
+	}
+
+	static void Main()
+	{
+		UseUnsafe();
+		UseFixed();
+	}
+}
+
+class UseFixedCode
+{
+	public int num;
+
+	public UseFixedCode(int num)
+	{
+		this.num = num;
+	}	
+}
