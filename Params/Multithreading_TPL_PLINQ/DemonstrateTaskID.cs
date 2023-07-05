39b04fe15f09fadff4784669bbaab985a9e2ea0c
Beka_Kavlelashvili <beqakavlela2001@gmail.com>
2023-07-05 15:13:42 +0400

    demonstrate task id

---
+++ Params/Multithreading_TPL_PLINQ/DemonstrateTaskID.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Multithreading_TPL_PLINQ
+{
+    public class DemonstrateTaskID
+    {
+        static void MyTask()
+        {
+            Console.WriteLine("MyTask() #" + Task.CurrentId + " starting");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(500);
+                Console.WriteLine("In MyTask() #" + Task.CurrentId + ", count is " + i);
+            }
+
+            Console.WriteLine("MyTask #" + Task.CurrentId + " terminating");
+        }
+
+        public static void DemonstrateTaskIDMain()
+        {
+            Console.WriteLine("\n=== Demonstrate Task ID ===");
+
+            Console.WriteLine("Main Task starting.");
+
+            Task tsk1 = new Task(MyTask);
+            Task tsk2 = new Task(MyTask);
+
+            tsk1.Start();
+            tsk2.Start();
+
+            Console.WriteLine("TaskId for tsk1: " + tsk1.Id);
+            Console.WriteLine("TaskId for tsk2: " + tsk2.Id);
+
+            for (int i = 0; i < 60; i++)
+            {
+                Console.Write(".");
+                Thread.Sleep(100);
+            }
+
+            Console.WriteLine("Main thread ending");
+        }
+    }
+}
--- Params/Multithreading_TPL_PLINQ/Program.cs
+++ Params/Multithreading_TPL_PLINQ/Program.cs
@@ -5,5 +5,6 @@ class Program
     static void Main()
     {
         CreateAndRunTask.CreateAndRunTaskMain();
+        DemonstrateTaskID.DemonstrateTaskIDMain();
     }
 }
